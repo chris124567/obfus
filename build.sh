a9e5b9e9f2763e7567d6aa5460148fe0a99cf052
Christopher Tarry <chrisjtarry@gmail.com>
2021-08-10 20:40:13 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -eux
 CFLAGS="-fno-rtti -fPIC -shared -std=c++17 -fdata-sections -ffunction-sections"
 CFLAGS="$CFLAGS -DDEBUG=1"
 CFLAGS="$CFLAGS -Wall -Wextra -Wstrict-prototypes -pedantic -Wno-unused-parameter"
-CFLAGS="$CFLAGS -Wl,--no-export-dynamic -Wl,--no-omagic -Wl,-z,nodelete -Wl,-z,nodump -Wl,--no-demangle -Wl,--gc-sections -Wl,--as-needed -Wl,--relax -Wl,--disable-new-dtags -Wl,--hash-style=sysv -Wl,--build-id=none -Wl,--no-eh-frame-hdr -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,now -Wl,-z,noseparate-code"
 CFLAGS="$CFLAGS -flto -Ofast -march=native -fmerge-all-constants"
 # CFLAGS="$CFLAGS -fsanitize=leak"
 clang-format-11 -i -style="{BasedOnStyle: Google, IndentWidth: 4, ColumnLimit: 0}" *.cpp *.hpp test/*.c
